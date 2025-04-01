36f7a76b4a50aa646b346325819157b7325877d2
Swarna Sri Bhanu <swarnasribhanu1@gmail.com>
2025-04-01 10:50:30 +0530

    navbar.php

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<div class="navbar">
+    <a href="index.html">Home</a>
+    <a href="abstract.html">Abstract</a>
+    <a href="existed-proposed-system.html">Existed and Proposed System</a>
+    <a href="survey-data.html">Survey Data</a>
+    <a href="graphs.html">Graphs</a>
+    <a href="survey-photos.html">Survey Photos</a>
+    <a href="team-members.html">Team Members</a>
+    <a href="conclusion.html">Conclusion</a>
+</div>
